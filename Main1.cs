431c9cc0038171400b4613072dfb0c55589f3535
Ethan Buck <ST10250745@vcconnect.edu.za>
2024-04-16 08:23:08 +0200

    Created 2 classes

--- Main1.cs
+++ Main1.cs
@@ -4,13 +4,7 @@ namespace PROGPOE
 {
     internal class Main1
     {
-        static void Main(string[] args)
-        {
-            Classes.Class1 clsl1 = new Classes.Class1();
-
-            clsl1.Run();
-
-        }
+        
     }
 }
 
