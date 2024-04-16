a19c95ecc6098359b9c1f3971df35dc7a42964b2
Ethan Buck <ST10250745@vcconnect.edu.za>
2024-04-16 08:28:46 +0200

    Initialize all variables

--- Class1.cs
+++ Class1.cs
@@ -1,9 +1,27 @@
-﻿using System;
+﻿// Ethan Buck
+// ST10250745
+// Group 1
+
+using System;
 
 namespace PROGPOE.Classes
 {
     internal class Class1
     {
+        List<string> ingName = new List<string>();
+        List<int> ingQuantity = new List<int>();
+        List<string> ingUnit = new List<string>();
+        List<string> stepDescription = new List<string>();
+        List<int> newIngQuantity = new List<int>();
+
+        string name;
+        int quantity;
+        string unit;
+        int numSteps;
+        string description;
+        double numScale;
+        int newQuantity;
+        int numIngredients;
 
     }
 }
--- Main1.cs
+++ Main1.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿// Ethan Buck
+// ST10250745
+// Group 1
+
+using System;
 
 namespace PROGPOE
 {
     internal class Main1
     {
-        
+        static void Main(string[] args)
+        {
+            
+        }
+
     }
 }
 
